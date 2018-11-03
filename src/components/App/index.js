4092aef8c2121f3312c94128c7796d5d34421f81
Jean-Denis Vauguet <jd@vauguet.fr>
2018-11-04 00:27:50 +0100

    Tests using OpenWhisk (CORS support namely)

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,6 +2,7 @@
  * Import
  */
 import React from 'react';
+import axios from 'axios';
 
 /**
  * Local import
@@ -14,12 +15,36 @@ import './app.sass';
 /**
  * Code
  */
-const App = () => (
-  <div id="app">
-    <h1 id="app-title">Codering</h1>
-    <p id="app-content">Hello tout le monde !!!</p>
-  </div>
-);
+const API_URL = 'https://172.17.0.1/api/v1/web';
+
+class App extends React.Component {
+  componentDidMount() {
+    console.log('test');
+    axios
+      // .post('http://192.168.99.100:31883', {
+      // trying to work around CORS limitation in Nuclio, using both:
+      // - dc up
+      // - telepresence --swap-deployment codering-ui --namespace codering --expose 3000:3000 --run-shell
+      // as reported by kubectl get service codering-ui --namespace codering
+      // but that's no better for the local app is still running from localhost:3000
+      .post(`${API_URL}/guest/default/greeting.json`, {
+        name: 'jd'
+      }, {
+        crossDomain: true
+      })
+      .then(console.log)
+      .catch(console.error);
+  }
+
+  render() {
+    return (
+      <div id="app">
+        <h1 id="app-title">Codering</h1>
+        <p id="app-content">Hello tout le monde !!!</p>
+      </div>
+    );
+  }
+};
 
 /**
  * Export
